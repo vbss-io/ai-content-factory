0b983b7961b79542c1c6be3432de32d2a6c33de7
Vitor Bastos <vbss-services@outlook.com>
2024-09-21 16:33:00 -0300

    feat: database connection error to error catalog

--- src/infra/error/ErrorCatalog.ts
+++ src/infra/error/ErrorCatalog.ts
@@ -1,6 +1,12 @@
 import { CustomError } from '@/infra/error/CustomError'
 import { HttpStatusCodes } from '@/infra/http/HttpStatusCodes'
 
+export class DatabaseConnectionError extends CustomError {
+  constructor () {
+    super(HttpStatusCodes.InternalServerError, 'Database Connection Error')
+  }
+}
+
 export class NotFoundError extends CustomError {
   constructor () {
     super(HttpStatusCodes.NotFound, 'Not found')
