118b6ec43637cbac60cc96a3c2809c1dd8bd4db1
Vitor Bastos <vbss-services@outlook.com>
2024-09-21 08:41:23 -0300

    feat: httpclient axios adapter

--- src/infra/http/HttpClient.ts
+++ src/infra/http/HttpClient.ts
@@ -1,3 +1,9 @@
+import axios from 'axios'
+
+axios.defaults.validateStatus = function () {
+  return true
+}
+
 export interface HttpClientGetInput {
   url: string
   params?: any
@@ -6,7 +12,7 @@ export interface HttpClientGetInput {
 
 export interface HttpClientPostInput {
   url: string
-  body: any
+  body?: any
   params?: any
   headers?: any
 }
@@ -17,4 +23,31 @@ export interface HttpClient {
   put: ({ url, body, params, headers }: HttpClientPostInput) => Promise<any>
   patch: ({ url, body, params, headers }: HttpClientPostInput) => Promise<any>
   delete: ({ url, params, headers }: HttpClientGetInput) => Promise<any>
+}
+
+export class AxiosAdapter implements HttpClient {
+  async get ({ url, params = {}, headers = {} }: HttpClientGetInput): Promise<any> {
+    const response = await axios.get(url, { params, headers })
+    return response.data
+  }
+
+  async post ({ url, body = {}, params = {}, headers = {} }: HttpClientPostInput): Promise<any> {
+    const response = await axios.post(url, body, { params, headers })
+    return response.data
+  }
+
+  async put ({ url, body = {}, params = {}, headers = {} }: HttpClientPostInput): Promise<any> {
+    const response = await axios.put(url, body, { params, headers })
+    return response.data
+  }
+
+  async patch ({ url, body = {}, params = {}, headers = {} }: HttpClientPostInput): Promise<any> {
+    const response = await axios.patch(url, body, { params, headers })
+    return response.data
+  }
+
+  async delete ({ url, params = {}, headers = {} }: HttpClientGetInput): Promise<any> {
+    const response = await axios.delete(url, { params, headers })
+    return response.data
+  }
 }
