e1a2afa9ceed00dc9650e8bc2b698761bf195510
Vitor Bastos <vbss-services@outlook.com>
2024-09-22 10:01:21 -0300

    feat: update image module and main file

--- src/application/modules/ImageModule.ts
+++ src/application/modules/ImageModule.ts
@@ -1,21 +1,32 @@
-import { ImagineController } from '@/application/controllers/ImageController'
-import { ImagineImage } from '@/application/usecases/ImagineImage'
-import { Registry } from '@/infra/dependency-injection/Registry'
+import { ImageController } from '@/application/controllers/ImageController'
+import { ProcessImage } from '@/application/usecases/ProcessImage'
+import { RequestImage } from '@/application/usecases/RequestImage'
+import { inject, Registry } from '@/infra/dependency-injection/Registry'
 import { StableDiffusionGatewayHttp } from '@/infra/gateways/ImagineImageGateway'
+import { BatchRepositoryMongo } from '@/infra/mongodb/repository/BatchRepositoryMongo'
 import { ImageRepositoryMongo } from '@/infra/mongodb/repository/ImageRepositoryMongo'
-import { ImagineImageSchema } from '@/infra/schemas/ImagineImageSchema'
+import { type Queue } from '@/infra/queue/Queue'
+import { RequestImageSchema } from '@/infra/schemas/RequestImageSchema'
 import { ZodAdapter } from '@/infra/validate/InputValidate'
 
 export class ImageModule {
+  @inject('queue')
+  private readonly queue!: Queue
+
   constructor () {
-    const imagineImageValidate = new ZodAdapter(ImagineImageSchema)
-    Registry.getInstance().provide('imagineImageValidate', imagineImageValidate)
+    void this.queue.register('imageRequested', 'imageRequested.processImage')
+    const requestImageValidate = new ZodAdapter(RequestImageSchema)
+    Registry.getInstance().provide('requestImageValidate', requestImageValidate)
     const imagineImageGateway = new StableDiffusionGatewayHttp()
     Registry.getInstance().provide('imagineImageGateway', imagineImageGateway)
     const imageRepository = new ImageRepositoryMongo()
+    const batchRepository = new BatchRepositoryMongo()
     Registry.getInstance().provide('imageRepository', imageRepository)
-    const imagineImage = new ImagineImage()
-    Registry.getInstance().provide('imagineImage', imagineImage)
-    new ImagineController()
+    Registry.getInstance().provide('batchRepository', batchRepository)
+    const requestImage = new RequestImage()
+    const processImage = new ProcessImage()
+    Registry.getInstance().provide('requestImage', requestImage)
+    Registry.getInstance().provide('processImage', processImage)
+    new ImageController()
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -9,25 +9,30 @@ import { AxiosAdapter } from '@/infra/http/HttpClient'
 import { ExpressAdapter, type HttpServer } from '@/infra/http/HttpServer'
 import { LocalImageStorageAdapter } from '@/infra/storage/ImageStorage'
 import { MongooseAdapter } from './infra/database/DatabaseConnection'
+import { RabbitMQAdapter } from './infra/queue/Queue'
 
 const isTestEnvironment = process.env.NODE_ENV === 'test'
 let httpServer: HttpServer
 
-function main (): any {
+async function main (): Promise<any> {
   const errorHandler = new ExpressErrorHandler()
   Registry.getInstance().provide('errorHandler', errorHandler)
   httpServer = new ExpressAdapter()
   const httpClient = new AxiosAdapter()
   const imageStorage = new LocalImageStorageAdapter()
+  const queue = new RabbitMQAdapter()
+  await queue.connect()
   const databaseConnection = new MongooseAdapter(String(process.env.MONGO_URI))
+  await databaseConnection.connect()
   Registry.getInstance().provide('httpServer', httpServer)
   Registry.getInstance().provide('httpClient', httpClient)
   Registry.getInstance().provide('imageStorage', imageStorage)
+  Registry.getInstance().provide('queue', queue)
   Registry.getInstance().provide('databaseConnection', databaseConnection)
   new StatusModule()
   new ImageModule()
   if (!isTestEnvironment) void httpServer.start(Number(process.env.PORT))
 }
-main()
+void main()
 
 export { httpServer }
