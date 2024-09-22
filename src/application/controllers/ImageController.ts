ee678d8ad71149a86863d68533713f252ca13b22
Vitor Bastos <vbss-services@outlook.com>
2024-09-22 10:00:12 -0300

    feat: register queue consumer for image controller

--- src/application/controllers/ImageController.ts
+++ src/application/controllers/ImageController.ts
@@ -1,24 +1,37 @@
-import { type ImagineImage } from '@/application/usecases/ImagineImage'
+import { type ProcessImage } from '@/application/usecases/ProcessImage'
+import { type RequestImage } from '@/application/usecases/RequestImage'
+import { type ImageRequestedData } from '@/domain/events/ImageRequested'
 import { inject } from '@/infra/dependency-injection/Registry'
 import type { HttpServer } from '@/infra/http/HttpServer'
 import { HttpStatusCodes } from '@/infra/http/HttpStatusCodes'
-import { type ImagineImageInput } from '@/infra/schemas/ImagineImageSchema'
+import { type Queue } from '@/infra/queue/Queue'
+import { type RequestImageInput } from '@/infra/schemas/RequestImageSchema'
 import { type InputValidate } from '@/infra/validate/InputValidate'
 
-export class ImagineController {
+export class ImageController {
   @inject('httpServer')
   private readonly httpServer!: HttpServer
 
-  @inject('imagineImage')
-  private readonly imagineImage!: ImagineImage
+  @inject('queue')
+  private readonly queue!: Queue
 
-  @inject('imagineImageValidate')
-  private readonly imagineImageValidate!: InputValidate<ImagineImageInput>
+  @inject('requestImageValidate')
+  private readonly requestImageValidate!: InputValidate<RequestImageInput>
+
+  @inject('requestImage')
+  private readonly requestImage!: RequestImage
+
+  @inject('processImage')
+  private readonly processImage!: ProcessImage
 
   constructor () {
-    this.httpServer.register('post', '/image/imagine', async (params: ImagineImageInput) => {
-      const inputParsed = this.imagineImageValidate.validate(params)
-      return await this.imagineImage.execute(inputParsed)
-    }, HttpStatusCodes.OK)
+    this.httpServer.register('post', '/image/imagine', async (params: RequestImageInput) => {
+      const inputParsed = this.requestImageValidate.validate(params)
+      return await this.requestImage.execute(inputParsed)
+    }, HttpStatusCodes.Created)
+
+    void this.queue.consume('imageRequested.processImage', async (input: ImageRequestedData) => {
+      await this.processImage.execute(input)
+    })
   }
 }
