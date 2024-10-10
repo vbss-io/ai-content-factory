5126fcce3beeea75ad3c230d409a466fe1427877
Vitor Bastos <vbss-services@outlook.com>
2024-10-10 09:23:47 -0300

    feat: register missing dependencies for main

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,33 @@
 import 'dotenv/config'
 import 'express-async-errors'
 
+import { BatchModule } from '@/application/modules/BatchModule'
 import { ImageModule } from '@/application/modules/ImageModule'
 import { StatusModule } from '@/application/modules/StatusModule'
+import { UserModule } from '@/application/modules/UserModule'
+import { MongooseAdapter } from '@/infra/database/DatabaseConnection'
 import { Registry } from '@/infra/dependency-injection/Registry'
 import { ExpressErrorHandler } from '@/infra/error/ErrorHandler'
 import { AxiosAdapter } from '@/infra/http/HttpClient'
 import { ExpressAdapter, type HttpServer } from '@/infra/http/HttpServer'
-import { LocalImageStorageAdapter } from '@/infra/storage/ImageStorage'
-import { MongooseAdapter } from './infra/database/DatabaseConnection'
-import { RabbitMQAdapter } from './infra/queue/Queue'
+import { RabbitMQAdapter } from '@/infra/queue/Queue'
+import { AzureStorageAdapter } from '@/infra/storage/ImageStorage'
+import { ExpressAuthHandler } from './infra/auth/AuthHandler'
+import { JWTAdapter } from './infra/auth/TokenAuthentication'
 
 const isTestEnvironment = process.env.NODE_ENV === 'test'
 let httpServer: HttpServer
 
 async function main (): Promise<any> {
   const errorHandler = new ExpressErrorHandler()
+  const tokenAuthentication = new JWTAdapter()
+  const authHandler = new ExpressAuthHandler()
+  Registry.getInstance().provide('tokenAuthentication', tokenAuthentication)
   Registry.getInstance().provide('errorHandler', errorHandler)
+  Registry.getInstance().provide('authHandler', authHandler)
   httpServer = new ExpressAdapter()
   const httpClient = new AxiosAdapter()
-  const imageStorage = new LocalImageStorageAdapter()
+  const imageStorage = new AzureStorageAdapter()
   const queue = new RabbitMQAdapter()
   await queue.connect()
   const databaseConnection = new MongooseAdapter(String(process.env.MONGO_URI))
@@ -30,6 +38,8 @@ async function main (): Promise<any> {
   Registry.getInstance().provide('queue', queue)
   Registry.getInstance().provide('databaseConnection', databaseConnection)
   new StatusModule()
+  new UserModule()
+  new BatchModule()
   new ImageModule()
   if (!isTestEnvironment) void httpServer.start(Number(process.env.PORT))
 }
