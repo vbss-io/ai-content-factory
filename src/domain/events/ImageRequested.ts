16a1e82055d941945d3c185117408369213cdd15
Vitor Bastos <vbss-services@outlook.com>
2024-10-10 09:08:55 -0300

    feat: add gateway to imageRequested event domain

--- src/domain/events/ImageRequested.ts
+++ src/domain/events/ImageRequested.ts
@@ -3,12 +3,12 @@ import type { DomainEvent } from '@/domain/events/DomainEvent'
 export class ImageRequested implements DomainEvent {
   eventName = 'imageRequested'
 
-  constructor (readonly data: ImageRequestedData) {
-  }
+  constructor (readonly data: ImageRequestedData) {}
 }
 
 export interface ImageRequestedData {
   batchId: string
+  gateway: string
   dimensions: {
     width: number
     height: number
