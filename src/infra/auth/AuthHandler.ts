1c12706a60e7053d69ed88e74b946b9b673fe06a
Vitor Bastos <vbss-services@outlook.com>
2024-10-10 09:16:23 -0300

    feat: express middleware to deal with auth

---
+++ src/infra/auth/AuthHandler.ts
@@ -0,0 +1,36 @@
+import { inject } from '@/infra/dependency-injection/Registry'
+import { AuthError, MissingAuthorizationToken, NotAllowedError } from '@/infra/error/ErrorCatalog'
+import type { NextFunction, Request, Response } from 'express'
+import { type TokenAuthentication } from './TokenAuthentication'
+
+export interface AuthHandler {
+  handle: (req: any, res: any, next: any) => Promise<any>
+}
+
+export class ExpressAuthHandler implements AuthHandler {
+  protected byPassRoutes = [
+    { method: 'GET', path: '/status' },
+    { method: 'POST', path: '/sign_in' },
+    { method: 'POST', path: '/login' },
+    { method: 'GET', path: '/image' },
+    { method: 'GET', path: '/images' }
+  ]
+
+  @inject('tokenAuthentication')
+  private readonly tokenAuthentication!: TokenAuthentication
+
+  async handle (req: Request, _res: Response, next: NextFunction): Promise<void> {
+    const canByPass = this.byPassRoutes.find((route) => route.method === req.method && route.path === req.path)
+    if (canByPass) { next(); return }
+    const { authorization } = req.headers
+    const token = authorization?.replace('Bearer ', '')
+    if (!token) throw new MissingAuthorizationToken()
+    try {
+      const { role } = await this.tokenAuthentication.decode(token, process.env.SECRET_KEY as string)
+      if (!role || role !== 'administrator') throw new NotAllowedError()
+    } catch (error: any) {
+      throw new AuthError(error.message as string)
+    }
+    next()
+  }
+}
