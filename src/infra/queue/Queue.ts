ff05d2e2f87756e7fcaca9851c3f58c0e5bf20ef
Vitor Bastos <vbss-services@outlook.com>
2024-10-12 17:27:47 -0300

    feat: add a in memory queue adapter

--- src/infra/queue/Queue.ts
+++ src/infra/queue/Queue.ts
@@ -8,6 +8,60 @@ export interface Queue {
   consume: (queueName: string, callback: any) => Promise<void>
 }
 
+export class InMemoryQueueAdapter implements Queue {
+  private readonly queues: Map<string, any[]>
+
+  constructor () {
+    this.queues = new Map()
+  }
+
+  async connect (): Promise<void> {
+    console.log('Connected to in-memory queue')
+  }
+
+  async register (queueName: string): Promise<void> {
+    if (!this.queues.has(queueName)) {
+      this.queues.set(queueName, [])
+    }
+  }
+
+  async publish (queueName: string, data: any): Promise<void> {
+    if (this.queues.has(queueName)) {
+      const queue = this.queues.get(queueName)
+      if (queue) {
+        queue.push(data)
+      }
+    } else {
+      console.error(`Queue ${queueName} does not exist`)
+    }
+  }
+
+  async consume (queueName: string, callback: (data: any) => Promise<void>): Promise<void> {
+    if (!this.queues.has(queueName.split('.')[0])) {
+      console.error(`Queue ${queueName} does not exist`)
+      return
+    }
+    const queue = this.queues.get(queueName.split('.')[0])
+    const processQueue = async (): Promise<void> => {
+      if (queue) {
+        while (queue.length > 0) {
+          const data = queue.shift()
+          try {
+            await callback(data)
+          } catch (error) {
+            console.error('Error processing message:', error)
+          }
+        }
+      }
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    setInterval(async () => {
+      await processQueue()
+    }, 1000)
+  }
+}
+
 export class RabbitMQAdapter implements Queue {
   connection: any
 
