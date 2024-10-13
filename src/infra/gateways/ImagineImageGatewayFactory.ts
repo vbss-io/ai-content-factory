9bdbff116486613e6c47a4feb1825e9c922f5bbd
Vitor Bastos <vbss-services@outlook.com>
2024-10-13 10:56:09 -0300

    feat: add dalle 3 gateway to imagine factory

--- src/infra/gateways/ImagineImageGatewayFactory.ts
+++ src/infra/gateways/ImagineImageGatewayFactory.ts
@@ -1,12 +1,14 @@
 import { type ImagineImageGateway } from '@/domain/gateways/ImagineImageGateway'
 import { GatewayNotImplemented } from '@/infra/error/ErrorCatalog'
 import { Automatic1111GatewayHttp } from '@/infra/gateways/Automatic1111GatewayHttp'
-import { GoAPIMidjourneyGatewayHttp } from './GoAPIMidjourneyGatewayHttp'
+import { GoAPIMidjourneyGatewayHttp } from '@/infra/gateways/GoAPIMidjourneyGatewayHttp'
+import { OpenAIDalle3GatewayHttp } from '@/infra/gateways/OpenApiDalle3GatewayHttp'
 
 export class ImagineImageGatewayFactory {
   static create (gateway: string): ImagineImageGateway {
     if (gateway === 'automatic1111') return new Automatic1111GatewayHttp()
     if (gateway === 'goApiMidjourney') return new GoAPIMidjourneyGatewayHttp()
+    if (gateway === 'openAiDalle3') return new OpenAIDalle3GatewayHttp()
     throw new GatewayNotImplemented()
   }
 }
