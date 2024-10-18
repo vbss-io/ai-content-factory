0bcfe10dbefdcdfbf451505fa81216e04545b9b3
Vitor Bastos <vbss-services@outlook.com>
2024-10-18 08:20:51 -0300

    feat: register cron for main application

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import 'express-async-errors'
 
 import { BatchModule } from '@/application/modules/BatchModule'
 import { ImageModule } from '@/application/modules/ImageModule'
+import { PromptModule } from '@/application/modules/PromptModule'
 import { StatusModule } from '@/application/modules/StatusModule'
 import { UserModule } from '@/application/modules/UserModule'
+import { ExpressAuthHandler } from '@/infra/auth/AuthHandler'
+import { JWTAdapter } from '@/infra/auth/TokenAuthentication'
+import { CronAdapter } from '@/infra/cron/Cron'
 import { MongooseAdapter } from '@/infra/database/DatabaseConnection'
 import { Registry } from '@/infra/dependency-injection/Registry'
 import { ExpressErrorHandler } from '@/infra/error/ErrorHandler'
@@ -12,8 +16,6 @@ import { AxiosAdapter } from '@/infra/http/HttpClient'
 import { ExpressAdapter, type HttpServer } from '@/infra/http/HttpServer'
 import { InMemoryQueueAdapter } from '@/infra/queue/Queue'
 import { AzureStorageAdapter } from '@/infra/storage/ImageStorage'
-import { ExpressAuthHandler } from './infra/auth/AuthHandler'
-import { JWTAdapter } from './infra/auth/TokenAuthentication'
 
 const isTestEnvironment = process.env.NODE_ENV === 'test'
 let httpServer: HttpServer
@@ -32,17 +34,21 @@ async function main (): Promise<any> {
   await queue.connect()
   const databaseConnection = new MongooseAdapter(String(process.env.MONGO_URI))
   await databaseConnection.connect()
+  const cron = new CronAdapter()
   Registry.getInstance().provide('httpServer', httpServer)
   Registry.getInstance().provide('httpClient', httpClient)
   Registry.getInstance().provide('imageStorage', imageStorage)
   Registry.getInstance().provide('queue', queue)
   Registry.getInstance().provide('databaseConnection', databaseConnection)
+  Registry.getInstance().provide('cron', cron)
   new StatusModule()
   new UserModule()
   new BatchModule()
   new ImageModule()
+  new PromptModule()
   if (!isTestEnvironment) void httpServer.start(Number(process.env.PORT))
 }
 void main()
 
 export { httpServer }
+
