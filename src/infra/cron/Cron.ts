9d2972ed5b293b78c09ebd13ea46c2426953444b
Vitor Bastos <vbss-services@outlook.com>
2024-10-18 08:17:56 -0300

    feat: implement cron and cron dependence adapter

---
+++ src/infra/cron/Cron.ts
@@ -0,0 +1,32 @@
+import { AutomaticRequestImage } from '@/application/usecases/Image/AutomaticRequestImage'
+import { CronJob } from 'cron'
+
+export interface Cron {
+  start: () => Promise<void>
+  run: () => any
+}
+
+export class CronAdapter implements Cron {
+  cronJob: CronJob | undefined
+
+  async start (): Promise<void> {
+    console.log('cron starting')
+    const cron = CronJob.from({
+      cronTime: '0 */12 * * *',
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      onTick: this.run,
+      start: false,
+      timeZone: 'America/Sao_Paulo'
+    })
+    process.env.NODE_ENV !== 'test' && cron.start()
+  }
+
+  async run (): Promise<void> {
+    const automaticRequestImage = new AutomaticRequestImage()
+    try {
+      await automaticRequestImage.execute()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
