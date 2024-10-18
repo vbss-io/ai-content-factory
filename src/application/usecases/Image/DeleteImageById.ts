f775fd813d8bc54903f0afc5e57a0669461ea5af
Vitor Bastos <vbss-services@outlook.com>
2024-10-18 08:21:16 -0300

    feat: remove comments for delete images usecase

--- src/application/usecases/Image/DeleteImageById.ts
+++ src/application/usecases/Image/DeleteImageById.ts
@@ -1,4 +1,5 @@
 import { type DeleteImageByIdInput } from '@/application/usecases/Image/dto/DeleteImageById.dto'
+import { type Batch } from '@/domain/entities/Batch'
 import { type BatchRepository } from '@/domain/repository/BatchRepository'
 import { type ImageRepository } from '@/domain/repository/ImageRepository'
 import { inject } from '@/infra/dependency-injection/Registry'
@@ -18,14 +19,14 @@ export class DeleteImageById {
   async execute ({ id }: DeleteImageByIdInput): Promise<void> {
     const image = await this.imageRepository.getImageById(id)
     if (!image) throw new ImageNotFoundError()
-    // const batch = await this.batchRepository.getBatchById(image.batchId) as Batch
-    // batch?.removeImage(id)
-    // if (!batch?.images.length) {
-    //   await this.batchRepository.deleteById(batch?.id)
-    // } else {
-    //   await this.batchRepository.update(batch)
-    // }
-    // await this.imageRepository.deleteById(id)
+    const batch = await this.batchRepository.getBatchById(image.batchId) as Batch
+    batch?.removeImage(id)
+    if (!batch?.images.length) {
+      await this.batchRepository.deleteById(batch?.id)
+    } else {
+      await this.batchRepository.update(batch)
+    }
+    await this.imageRepository.deleteById(id)
     await this.imageStorage.deleteImage(image.path)
   }
 }
