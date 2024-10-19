1c3fdbd4abda4bfb474c8e380a4b783e8e88d1b7
Vitor Bastos <vbss-services@outlook.com>
2024-10-19 19:52:21 -0300

    feat: update image getImages repository input

--- src/domain/repository/ImageRepository.ts
+++ src/domain/repository/ImageRepository.ts
@@ -5,6 +5,7 @@ export interface ImageRepository {
   update: (image: Image) => Promise<void>
   deleteById: (id: string) => Promise<void>
   getImageById: (id: string) => Promise<Image | undefined>
-  getImages: (page: number, searchMask?: string) => Promise<Image[]>
+  getImages: (page: number, searchMask?: string, sampler?: string, scheduler?: string, aspectRatio?: string, origin?: string, modelName?: string) => Promise<Image[]>
   getRandomLandscapeImage: () => Promise<Image | undefined>
+  getImagesByIds: (ids: string[]) => Promise<Image[] | []>
 }
