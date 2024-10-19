88ef45da6ea59d1800d0bd470b754cad6178bc7f
Vitor Bastos <vbss-services@outlook.com>
2024-10-19 19:58:04 -0300

    feat: update get images usecase with filter options

--- src/application/usecases/Image/GetImages.ts
+++ src/application/usecases/Image/GetImages.ts
@@ -6,7 +6,7 @@ export class GetImages {
   @inject('imageRepository')
   private readonly imageRepository!: ImageRepository
 
-  async execute ({ search_mask, page }: GetImagesInput): Promise<GetImagesOutput> {
-    return await this.imageRepository.getImages(page, search_mask)
+  async execute ({ search_mask, sampler, scheduler, aspectRatio, origin, modelName, page }: GetImagesInput): Promise<GetImagesOutput> {
+    return await this.imageRepository.getImages(page, search_mask, sampler, scheduler, aspectRatio, origin, modelName)
   }
 }
--- src/application/usecases/Image/dto/GetImages.dto.ts
+++ src/application/usecases/Image/dto/GetImages.dto.ts
@@ -2,6 +2,11 @@ import { type Image } from '@/domain/entities/Image'
 
 export interface GetImagesInput {
   search_mask?: string
+  sampler?: string
+  scheduler?: string
+  aspectRatio?: string
+  origin?: string
+  modelName?: string
   page: number
 }
 
