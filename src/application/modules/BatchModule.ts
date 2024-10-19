7c249ef2a5ddbac5557dd123be9708261c8514b8
Vitor Bastos <vbss-services@outlook.com>
2024-10-19 19:59:18 -0300

    feat: update registers for image and batch

--- src/application/modules/BatchModule.ts
+++ src/application/modules/BatchModule.ts
@@ -2,7 +2,9 @@ import { BatchController } from '@/application/controllers/BatchController'
 import { DeleteBatchById } from '@/application/usecases/Batch/DeleteBatchById'
 import { GetBatchById } from '@/application/usecases/Batch/GetBatchById'
 import { GetBatches } from '@/application/usecases/Batch/GetBatches'
+import { GetBatchFilters } from '@/application/usecases/Batch/GetBatchFilters'
 import { Registry } from '@/infra/dependency-injection/Registry'
+import { GetBatchFiltersQueryMongo } from '@/infra/mongodb/queries/GetBatchFiltersMongo'
 import { BatchRepositoryMongo } from '@/infra/mongodb/repository/BatchRepositoryMongo'
 import { ByIdSchema } from '@/infra/schemas/ByIdSchema'
 import { GetAllSchema } from '@/infra/schemas/GetAllSchema'
@@ -16,12 +18,16 @@ export class BatchModule {
     Registry.getInstance().provide('getAllValidate', getAllValidate)
     const batchRepository = new BatchRepositoryMongo()
     Registry.getInstance().provide('batchRepository', batchRepository)
+    const getBatchFiltersQuery = new GetBatchFiltersQueryMongo()
+    Registry.getInstance().provide('getBatchFiltersQuery', getBatchFiltersQuery)
     const getBatchById = new GetBatchById()
     const deleteBatchById = new DeleteBatchById()
     const getBatches = new GetBatches()
+    const getBatchFilters = new GetBatchFilters()
     Registry.getInstance().provide('getBatchById', getBatchById)
     Registry.getInstance().provide('deleteBatchById', deleteBatchById)
     Registry.getInstance().provide('getBatches', getBatches)
+    Registry.getInstance().provide('getBatchFilters', getBatchFilters)
     new BatchController()
   }
 }
--- src/application/modules/ImageModule.ts
+++ src/application/modules/ImageModule.ts
@@ -1,11 +1,13 @@
 import { ImageController } from '@/application/controllers/ImageController'
 import { DeleteImageById } from '@/application/usecases/Image/DeleteImageById'
 import { GetImageById } from '@/application/usecases/Image/GetImageById'
+import { GetImageFilters } from '@/application/usecases/Image/GetImageFilters'
 import { GetImages } from '@/application/usecases/Image/GetImages'
 import { GetRandomLandscapeImage } from '@/application/usecases/Image/GetRandomLandscapeImage'
 import { ProcessImage } from '@/application/usecases/Image/ProcessImage'
 import { RequestImage } from '@/application/usecases/Image/RequestImage'
 import { inject, Registry } from '@/infra/dependency-injection/Registry'
+import { GetImageFiltersQueryMongo } from '@/infra/mongodb/queries/GetImageFiltersMongo'
 import { ImageRepositoryMongo } from '@/infra/mongodb/repository/ImageRepositoryMongo'
 import { type Queue } from '@/infra/queue/Queue'
 import { ByIdSchema } from '@/infra/schemas/ByIdSchema'
@@ -27,18 +29,22 @@ export class ImageModule {
     Registry.getInstance().provide('getAllValidate', getAllValidate)
     const imageRepository = new ImageRepositoryMongo()
     Registry.getInstance().provide('imageRepository', imageRepository)
+    const getImageFiltersQuery = new GetImageFiltersQueryMongo()
+    Registry.getInstance().provide('getImageFiltersQuery', getImageFiltersQuery)
     const requestImage = new RequestImage()
     const getImageById = new GetImageById()
     const deleteImageById = new DeleteImageById()
     const getImages = new GetImages()
     const getRandomLandscapeImage = new GetRandomLandscapeImage()
     const processImage = new ProcessImage()
+    const getImageFilters = new GetImageFilters()
     Registry.getInstance().provide('requestImage', requestImage)
     Registry.getInstance().provide('getImageById', getImageById)
     Registry.getInstance().provide('deleteImageById', deleteImageById)
     Registry.getInstance().provide('getImages', getImages)
     Registry.getInstance().provide('getRandomLandscapeImage', getRandomLandscapeImage)
     Registry.getInstance().provide('processImage', processImage)
+    Registry.getInstance().provide('getImageFilters', getImageFilters)
     new ImageController()
   }
 }
