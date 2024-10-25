b80bb8c84393c89b9e34facb97bf8fd13be8686c
Vitor Bastos <vbss-services@outlook.com>
2024-10-25 19:28:34 -0300

    feat: rename azure image adapter and add a video storage and azure adapter

--- src/infra/storage/VideoStorage.ts
+++ src/infra/storage/VideoStorage.ts
@@ -4,54 +4,54 @@ import crypto from 'crypto'
 import fs from 'fs'
 import path from 'path'
 
-export interface ImageStorage {
-  uploadImage: (base64Image: string) => Promise<string>
-  deleteImage: (filepath: string) => Promise<void>
+export interface VideoStorage {
+  uploadVideo: (base64Video: string) => Promise<string>
+  deleteVideo: (filepath: string) => Promise<void>
 }
 
-export class LocalImageStorageAdapter implements ImageStorage {
+export class LocalVideoStorageAdapter implements VideoStorage {
   private readonly uploadDir: string
 
   constructor () {
-    this.uploadDir = path.resolve(__dirname, '../../../images')
+    this.uploadDir = path.resolve(__dirname, '../../../videos')
     if (!fs.existsSync(this.uploadDir)) {
       fs.mkdirSync(this.uploadDir, { recursive: true })
     }
   }
 
-  async uploadImage (base64Image: string): Promise<string> {
+  async uploadVideo (base64Video: string): Promise<string> {
     const filename = crypto.randomUUID()
-    const imageBuffer = Buffer.from(base64Image, 'base64')
-    const finalFilename = `${filename}.png`
+    const videoBuffer = Buffer.from(base64Video, 'base64')
+    const finalFilename = `${filename}.mp4`
     const filePath = path.join(this.uploadDir, finalFilename)
-    await fs.promises.writeFile(filePath, imageBuffer)
-    return `/images/${finalFilename}`
+    await fs.promises.writeFile(filePath, videoBuffer)
+    return `/videos/${finalFilename}`
   }
 
-  async deleteImage (filepath: string): Promise<void> {
+  async deleteVideo (filepath: string): Promise<void> {
     throw new NotImplemented()
   }
 }
 
-export class AzureImageStorageAdapter implements ImageStorage {
+export class AzureVideoStorageAdapter implements VideoStorage {
   protected connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING as string
   protected containerName = process.env.AZURE_STORAGE_CONTAINER_NAME as string
 
-  async uploadImage (base64Image: string): Promise<string> {
+  async uploadVideo (base64Video: string): Promise<string> {
     const blobServiceClient = BlobServiceClient.fromConnectionString(this.connectionString)
     const containerClient = blobServiceClient.getContainerClient(this.containerName)
     const filename = crypto.randomUUID()
-    const finalFilename = `${filename}.png`
+    const finalFilename = `${filename}.mp4`
     const blockBlobClient = containerClient.getBlockBlobClient(finalFilename)
-    const imageBuffer = Buffer.from(base64Image, 'base64')
+    const videoBuffer = Buffer.from(base64Video, 'base64')
     await blockBlobClient.upload(
-      imageBuffer,
-      Buffer.byteLength(imageBuffer)
+      videoBuffer,
+      Buffer.byteLength(videoBuffer)
     )
     return `/${this.containerName}/${finalFilename}`
   }
 
-  async deleteImage (filepath: string): Promise<void> {
+  async deleteVideo (filepath: string): Promise<void> {
     const blobServiceClient = BlobServiceClient.fromConnectionString(this.connectionString)
     const containerClient = blobServiceClient.getContainerClient(this.containerName)
     const filename = filepath.replace(`/${this.containerName}/`, '')
