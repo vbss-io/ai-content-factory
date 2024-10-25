cefb21e965df751fa69d2e4e19a2e754df139ac2
Vitor Bastos <vbss-services@outlook.com>
2024-10-25 19:25:29 -0300

    feat: update user mongo model with image and video likes

--- src/infra/mongodb/model/UserModel.ts
+++ src/infra/mongodb/model/UserModel.ts
@@ -3,6 +3,8 @@ import mongoose, { type Document, type Model, Schema } from 'mongoose'
 export interface UserDocument extends Document {
   username: string
   hash: string
+  imageLikes: string[]
+  videoLikes: string[]
   role: string
   createdAt: Date
   updatedAt: Date
@@ -12,6 +14,8 @@ const UserSchema: Schema = new Schema(
   {
     username: { type: String, required: true, unique: true },
     hash: { type: String, required: true },
+    imageLikes: { type: [String], required: true, default: [] },
+    videoLikes: { type: [String], required: true, default: [] },
     role: { type: String, required: true }
   },
   { timestamps: true, versionKey: false }
