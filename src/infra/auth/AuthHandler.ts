ac2bb424b456aba46473a965241cba4ebd3bfbcc
Vitor Bastos <vbss-services@outlook.com>
2024-10-25 19:36:17 -0300

    feat: update routes to bypass auth

--- src/infra/auth/AuthHandler.ts
+++ src/infra/auth/AuthHandler.ts
@@ -1,5 +1,6 @@
 import { inject } from '@/infra/dependency-injection/Registry'
-import { AuthError, MissingAuthorizationToken, NotAllowedError } from '@/infra/error/ErrorCatalog'
+import { AuthError, MissingAuthorizationToken } from '@/infra/error/ErrorCatalog'
+import { type RequestFacade } from '@/infra/facade/RequestFacade'
 import type { NextFunction, Request, Response } from 'express'
 import { type TokenAuthentication } from './TokenAuthentication'
 
@@ -15,21 +16,28 @@ export class ExpressAuthHandler implements AuthHandler {
     { method: 'GET', path: '/image' },
     { method: 'GET', path: '/images' },
     { method: 'GET', path: '/image/banner' },
-    { method: 'GET', path: '/image/filters' }
+    { method: 'GET', path: '/image/filters' },
+    { method: 'GET', path: '/video' },
+    { method: 'GET', path: '/videos' },
+    { method: 'GET', path: '/video/filters' }
   ]
 
   @inject('tokenAuthentication')
   private readonly tokenAuthentication!: TokenAuthentication
 
+  @inject('requestFacade')
+  private readonly requestFacade!: RequestFacade
+
   async handle (req: Request, _res: Response, next: NextFunction): Promise<void> {
     const canByPass = this.byPassRoutes.find((route) => route.method === req.method && route.path === req.path)
-    if (canByPass) { next(); return }
     const { authorization } = req.headers
+    if (canByPass && !authorization) { next(); return }
     const token = authorization?.replace('Bearer ', '')
     if (!token) throw new MissingAuthorizationToken()
     try {
-      const { role } = await this.tokenAuthentication.decode(token, process.env.SECRET_KEY as string)
-      if (!role || role !== 'administrator') throw new NotAllowedError()
+      const { id, username, role } = await this.tokenAuthentication.decode(token, process.env.SECRET_KEY as string)
+      // if (!role || role !== 'administrator') throw new NotAllowedError()
+      this.requestFacade.setUser({ id, username, role })
     } catch (error: any) {
       throw new AuthError(error.message as string)
     }
