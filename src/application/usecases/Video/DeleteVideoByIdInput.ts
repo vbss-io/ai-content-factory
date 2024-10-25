da3d6fa3ad4aaeab28c1fe7b46446ea6ef4c7331
Vitor Bastos <vbss-services@outlook.com>
2024-10-25 19:34:35 -0300

    feat: delete videos by id usecase

---
+++ src/application/usecases/Video/DeleteVideoByIdInput.ts
@@ -0,0 +1,32 @@
+import { type Batch } from '@/domain/entities/Batch'
+import { type BatchRepository } from '@/domain/repository/BatchRepository'
+import { type VideoRepository } from '@/domain/repository/VideoRepository'
+import { inject } from '@/infra/dependency-injection/Registry'
+import { VideoNotFoundError } from '@/infra/error/ErrorCatalog'
+import { type VideoStorage } from '@/infra/storage/VideoStorage'
+import { type DeleteVideoByIdInput } from './dto/DeleteVideoByIdInput.dto'
+
+export class DeleteVideoById {
+  @inject('videoRepository')
+  private readonly videoRepository!: VideoRepository
+
+  @inject('batchRepository')
+  private readonly batchRepository!: BatchRepository
+
+  @inject('videoStorage')
+  private readonly videoStorage!: VideoStorage
+
+  async execute ({ id }: DeleteVideoByIdInput): Promise<void> {
+    const video = await this.videoRepository.getVideoById(id)
+    if (!video) throw new VideoNotFoundError()
+    const batch = await this.batchRepository.getBatchById(video.batchId) as Batch
+    batch?.removeVideo(id)
+    if (!batch?.videos.length) {
+      await this.batchRepository.deleteById(batch?.id)
+    } else {
+      await this.batchRepository.update(batch)
+    }
+    await this.videoRepository.deleteById(id)
+    await this.videoStorage.deleteVideo(video.path)
+  }
+}
---
+++ src/application/usecases/Video/dto/DeleteVideoByIdInput.dto.ts
@@ -0,0 +1,3 @@
+export interface DeleteVideoByIdInput {
+  id: string
+}
