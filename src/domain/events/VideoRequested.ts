f425615cd9129f1dfb38616c1890372e60f0eff8
Vitor Bastos <vbss-services@outlook.com>
2024-10-25 19:21:28 -0300

    feat: domai event for request videos

---
+++ src/domain/events/VideoRequested.ts
@@ -0,0 +1,18 @@
+import type { DomainEvent } from '@/domain/events/DomainEvent'
+
+export class VideoRequested implements DomainEvent {
+  eventName = 'videoRequested'
+
+  constructor (readonly data: VideoRequestedData) {}
+}
+
+export interface VideoRequestedData {
+  batchId: string
+  gateway: string
+  isAutomatic?: boolean
+  imageUrl: string
+  dimensions: {
+    width: number
+    height: number
+  }
+}
