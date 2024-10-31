ab5e9f4dd2e6159b378786f9b488938ed1869d61
Vitor Bastos <vbss-services@outlook.com>
2024-10-31 08:10:11 -0300

    feat(api): express errors handler with multer errors check

--- src/@api/infra/errors/ErrorHandler.ts
+++ src/@api/infra/errors/ErrorHandler.ts
@@ -4,9 +4,13 @@ import { ZodError } from 'zod'
 import { type ErrorHandler } from '@api/domain/errors/ErrorHandler'
 import { HttpStatusCodes } from '@api/domain/http/HttpStatusCodes'
 import { CustomError } from '@api/infra/errors/CustomError'
+import { MulterError } from 'multer'
 
 export class ExpressErrorHandler implements ErrorHandler {
   handle (err: Error, _req: Request, res: Response, _next: NextFunction): Response {
+    if (err instanceof MulterError) {
+      return res.status(HttpStatusCodes.BadRequest).json({ message: `${err.message}: ${err.field}. Files must be files.` })
+    }
     if (err instanceof ZodError) {
       return res.status(HttpStatusCodes.BadRequest).json({ message: err.errors[0].message })
     }
