118c3d853f7893487a069d3f6f4a85c1b08591b5
Vitor Bastos <vbss-services@outlook.com>
2024-11-15 12:42:47 -0300

    feat(image): add image repo domain with method to get only user images

--- src/image/domain/repositories/ImageRepository.ts
+++ src/image/domain/repositories/ImageRepository.ts
@@ -4,7 +4,8 @@ export interface ImageRepository {
   create: (image: Image) => Promise<Image>
   getImageById: (id: string) => Promise<Image | undefined>
   getImagesByIds: (ids: string[]) => Promise<Image[] | []>
-  getImages: (page: number, userId?: string, searchMask?: string, sampler?: string, scheduler?: string, aspectRatio?: string, origin?: string, modelName?: string) => Promise<Image[]>
+  getImages: (page: number, searchMask?: string, sampler?: string, scheduler?: string, aspectRatio?: string, origin?: string, modelName?: string) => Promise<Image[]>
+  getUserImages: (page: number, userId: string) => Promise<Image[]>
   getRandomLandscapeImage: () => Promise<Image | undefined>
   update: (image: Image) => Promise<void>
   deleteById: (id: string) => Promise<void>
