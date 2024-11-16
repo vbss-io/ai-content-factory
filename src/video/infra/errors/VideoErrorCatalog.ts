23176677178fcfaa1af4f12f08e7c614254028f6
Vitor Bastos <vbss-services@outlook.com>
2024-11-16 19:43:32 -0300

    feat(video): add error for luma labs dimensions error

--- src/video/infra/errors/VideoErrorCatalog.ts
+++ src/video/infra/errors/VideoErrorCatalog.ts
@@ -12,3 +12,9 @@ export class VideoNotFoundError extends CustomError {
     super(HttpStatusCodes.NotFound, 'Video Not found')
   }
 }
+
+export class LumaLabsDimensionsError extends CustomError {
+  constructor () {
+    super(HttpStatusCodes.BadRequest, 'Luma Labs aspectRatios allowed: 1:1, 16:9, 9:16, 4:3, 3:4, 21:9 or 9:21')
+  }
+}
